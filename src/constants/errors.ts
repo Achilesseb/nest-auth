9ab914a121ab05737e5374b5748310676c34f42a
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-02-04 21:47:57 +0200

    chore: extract constants/ refactor code

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -2,4 +2,5 @@ export const ERROR_MESSAGES = {
   USER_ALREADY_EXISTS: 'A user with this email already exist.',
   USER_DOESNT_EXISTS:
     'The user with the provided email address does not exists!',
+  WRONG_CREDENTIALS: 'Wrong credentials',
 };
